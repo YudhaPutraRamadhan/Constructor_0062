fd77118053c24e4a79fdcf880876463a273fd563
MzSuwawa <yudharamadhan838@gmail.com>
2024-05-13 18:03:23 +0700

    Menambahkan class untuk buku dan membuat prosedur utama

---
+++ ChainFnc/ChainFnc.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class buku
+{
+	string judul;
+
+public:
+	buku setjudul(string judul)
+	{
+		this->judul = judul;
+		return *this;
+	}
+	string getJudul()
+	{
+		return this->judul;
+	}
+} bukunya;
+
+int main()
+{
+	// bukunya.setjudul("Matematika");
+	// cout << bukunya.getJudul();
+	cout << bukunya.setjudul("Matematika").getJudul();
+	return 0;
+}
